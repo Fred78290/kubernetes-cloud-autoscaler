882b9832886f2ddf1a9f96ad48aa5159513561df
Fred78290 <frederic.boltz@gmail.com>
2024-02-16 15:51:00 +0100

    TEST: Add some test configuration

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -15,12 +15,16 @@ else
 	exit 1
 fi
 
+AUTOSCALER_DESKTOP_UTILITY_TLS=$(dirname $(kubernetes-desktop-autoscaler-utility certificate generate | jq -r .ClientKey) | sed -e 's/\//\\\//g')
+
 if [ -n "${NODEGROUP}" ]; then
 	mkdir -p "${CONFIG_DIR}"
 
 	cp ${AUTOSCALER_HOME}/cluster/${NODEGROUP}/config ${CONFIG_DIR}/config
-	cp ${AUTOSCALER_HOME}/config/${NODEGROUP}/config/provider.json ${CONFIG_DIR}/provider.json
 	cp ${AUTOSCALER_HOME}/config/${NODEGROUP}/config/machines.json ${CONFIG_DIR}/machines.json
+
+	cat ${AUTOSCALER_HOME}/config/${NODEGROUP}/config/provider.json | sed \
+		-e "s/\/etc\/ssl\/certs\/autoscaler-utility/${AUTOSCALER_DESKTOP_UTILITY_TLS}/g" > ${CONFIG_DIR}/provider.json
 
 	cat ${AUTOSCALER_HOME}/config/${NODEGROUP}/config/autoscaler.json | jq \
 		--arg ETCD_SSL_DIR "${AUTOSCALER_HOME}/cluster/${NODEGROUP}/etcd" \
